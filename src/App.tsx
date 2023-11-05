2d9a2a506c7b1a0f63e45b62e2cb2a93a70711d3
mahmudul474 <mdmahmudulla474@gmail.com>
2023-11-06 05:06:24 +0600

    assinment 5 start start and basic page and route done

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div    className='bg-black text-white '>goo morning  bangladesh </div>
+    <h1 className='bg-black  text-red-600'>hello developer </h1>
+    </>
+  )
+}
+
+export default App
