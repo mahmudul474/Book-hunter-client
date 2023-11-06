12457c5d4d5095e260018801ac6ff5bf1bc7f2e7
mahmudul474 <mdmahmudulla474@gmail.com>
2023-11-06 10:33:46 +0600

    main layot setup and  header and footer add

--- src/Layot/MainLayot.tsx
+++ src/Layot/MainLayot.tsx
@@ -1,14 +1,14 @@
 import { Outlet } from "react-router-dom";
-import Header from "../shared/Header";
+
 import Footer from "../shared/Footer";
+import { Header } from "../shared/Header";
 
 export default function MainLayot() {
   return (
     <>
-    <Header/>
-    <Outlet></Outlet>
-    <Footer></Footer>
-    
+      <Header />
+      <Outlet></Outlet>
+      <Footer></Footer>
     </>
-  )
+  );
 }
